4dbd67b24b4770cdb668d117631cbab616a78ca2
Justin Lamb <justinlamb@me.com>
2022-10-26 11:22:08 -0700

    flight search form and results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,12 @@
 class FlightsController < ApplicationController
+  before_action :set_airport_options
+
   def index
     @flights = Flight.all
+    @searched_flights = Flight.where(departure_id: params[:departure_id], arrival_id: params[:arrival_id])
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @flight = Flight.new
@@ -18,5 +20,9 @@ class FlightsController < ApplicationController
 
   def flight_params
     params.require(:flight).permit(:departure_id, :arrival_id, :departure_time, :duration)
+  end
+
+  def set_airport_options
+    @airport_options = Airport.all.map { |a| [a.code, a.id] }
   end
 end
